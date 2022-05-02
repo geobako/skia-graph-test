81847e6be9613c293235bf9c6a9550009595d318
geobako <giorgosbakogiannis@gmail.com>
2022-05-02 15:04:10 +0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,79 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React from 'react';
+import {SafeAreaView, StyleSheet, View} from 'react-native';
+import {gestureHandlerRootHOC} from 'react-native-gesture-handler';
+import gaussian from 'gaussian';
+import {LineGraph} from './src/LineGraph';
+import {Canvas, Text} from '@shopify/react-native-skia';
+import Animated, {
+  ComplexAnimationBuilder,
+  useSharedValue,
+} from 'react-native-reanimated';
+import {ReText} from 'react-native-redash';
+
+function weightedRandom(mean: number, variance: number): number {
+  var distribution = gaussian(mean, variance);
+  // Take a random sample using inverse transform sampling method.
+  return distribution.ppf(Math.random());
+}
+
+export function generateRandomGraphData(length: number): any[] {
+  return Array<number>(length)
+    .fill(0)
+    .map((_, index) => ({
+      date: new Date(index),
+      value: weightedRandom(10, Math.pow(index + 1, 2)),
+    }));
+}
+
+const App = () => {
+  const animatedText = useSharedValue('Hello');
+  const points = generateRandomGraphData(500);
+
+  const onPointSelect = point => {
+    // alert(point.value);
+    // console.log(444, point);
+    animatedText.value = `${point.value}`;
+  };
+
+  return (
+    <SafeAreaView style={{flex: 1}}>
+      <View style={styles.root}>
+        <ReText text={animatedText} />
+
+        <LineGraph
+          onPointSelected={onPointSelect}
+          style={styles.graph}
+          animated={true}
+          color="#3B9AD0"
+          points={points}
+          enablePanGesture={true}
+          // enableFadeInMask={true}
+          onGestureStart={() => console.log('helllo')}
+          selectionDotShadowColor={'#000000'}
+        />
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  root: {
+    paddingHorizontal: 24,
+    height: 300,
+  },
+  graph: {
+    height: 300,
+  },
+});
+
+export default gestureHandlerRootHOC(App);
